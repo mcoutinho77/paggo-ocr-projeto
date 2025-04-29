ecc397317430b13963111d03e131b57c8d8dd50f
mcoutinho77 <matheus.breno2023@outlook.com>
2025-04-28 22:48:05 -0300

    Atualiza o código para o deploy no Vercel

---
+++ frontend/src/app/results/[documentId]/page.tsx
@@ -0,0 +1,8 @@
+export default function DocumentPage() {
+    return (
+      <div>
+        Página de Documento
+      </div>
+    );
+  }
+  
---
+++ frontend/src/app/upload/page.tsx
@@ -0,0 +1,8 @@
+export default function UploadPage() {
+    return (
+      <div>
+        Página de Upload
+      </div>
+    );
+  }
+  
